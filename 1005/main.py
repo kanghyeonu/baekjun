0c8c78c0d8cc0a268e60fd20517800e6c83fceac
KHW <51577531+kanghyeonu@users.noreply.github.com>
2022-07-26 17:27:09 +0900

    _

--- 1005/main.py
+++ 1005/main.py
@@ -1,19 +1,34 @@
 import sys
+from collections import deque
 
+def topology_sort():
+    dp = [0] * (N+1)
+    q = deque()
 
-def dfs(start):
+    for i in range(1, N+1):
+        if indegree[i] == 0:
+            q.append(i)
+            dp[i] += D[i]
 
+    while q:
+        start = q.popleft()
+        for i in graph[start]:
+            indegree[i] -= 1
+            dp[i] = max(dp[i], dp[start]+D[i])
+            if indegree[i] == 0:
+                q.append(i)
+    return dp[obj]
 
 T = int(sys.stdin.readline().strip())
-
 for _ in range(T):
     N, K = map(int, sys.stdin.readline().split())
-    D = list(map(int, sys.stdin.readline().split()))
-    lst = []
+    D = [0] + list(map(int, sys.stdin.readline().split()))
+    graph = [[] for _ in range(N+1)]
+    indegree = [0] * (N+1)
     for i in range(K):
         X, Y = map(int, sys.stdin.readline().split())
-        lst.append((X, Y))
-    obj = int(sys.stdin.readline().split())
-
-    dfs()
-
+        graph[X].append(Y)
+        indegree[Y] +=1
+    obj = int(sys.stdin.readline().strip())
+    sol = topology_sort()
+    print(sol)
