64e182ee7e5a85909736a150a643f2a747a11c06
KHW <51577531+kanghyeonu@users.noreply.github.com>
2022-08-16 09:10:11 +0900

    _

--- 16139/main.py
+++ 16139/main.py
@@ -1,20 +1,30 @@
 import sys
 
-string = sys.stdin.readline().strip()
-#keys = list(set(string))
-q = int(sys.stdin.readline().strip())
+###-------------------------------------------------------------------###
+# string = sys.stdin.readline().strip()
+# q = int(sys.stdin.readline().strip())
+# for _ in range(q):
+#     char, s, e = map(str, sys.stdin.readline().split())
+#     s, e = int(s), int(e)
+#
+#     print(string[s:e + 1].count(char))
+###-------------------------------------------------------------------###
 
-for _ in range(q):
-    char, s, e = sys.stdin.readline().split()
-    s, e = int(s), int(e)
-    if char == string[0]:
-        dp = [0, 1]
-    else:
-        dp = [0, 0]
-    for i in range(1, len(string)):
-        if char == string[i]:
-            dp.append(dp[i-1] + 1)
+def makeTable(string, cha):
+    dp = [0]
+    for i in range(len(string)):
+        if string[i] == cha:
+            dp.append(dp[i] + 1)
         else:
-            dp.append(dp[i-1])
-    print(dp[e+1] - dp[s])
+            dp.append(dp[i])
+    return dp
 
+string = sys.stdin.readline().strip()
+q = int(sys.stdin.readline().strip())
+dic = {}
+for _ in range(q):
+    cha, s, e = map(str, sys.stdin.readline().split())
+    if cha not in dic.keys():
+        dic[cha] = makeTable(string ,cha)
+    s, e = int(s), int(e)
+    print(dic[cha][e+1] - dic[cha][s])
