7d66fed2a5e4192143e0c558e82619c80bebe7db
KHW <51577531+kanghyeonu@users.noreply.github.com>
2022-11-06 20:58:16 +0900

    _

---
+++ 10866/main.py
@@ -0,0 +1,43 @@
+import sys
+from collections import deque
+
+N = int(sys.stdin.readline().strip())
+
+q = deque()
+for _ in range(N):
+    command = sys.stdin.readline().strip()
+    if ' ' in command:
+        command, val = command.split()
+    if command == 'push_front':
+        q.appendleft(val)
+
+    elif command == 'push_back':
+        q.append(val)
+
+    elif command == 'pop_front':
+        if len(q) > 0:
+            print(q.popleft())
+        else:
+            print(-1)
+    elif command == 'pop_back':
+        if len(q) > 0:
+            print(q.pop())
+        else:
+            print(-1)
+    elif command == 'size':
+        print(len(q))
+    elif command == 'empty':
+        if len(q) == 0:
+            print(1)
+        else:
+            print(0)
+    elif command == 'front':
+        if len(q) > 0:
+            print(q[0])
+        else:
+            print(-1)
+    elif command == 'back':
+        if len(q) > 0:
+            print(q[-1])
+        else:
+            print(-1)
