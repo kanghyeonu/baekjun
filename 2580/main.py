8179050393855766c2a80ba7004ded731aade5de
KHW <51577531+kanghyeonu@users.noreply.github.com>
2023-01-02 15:26:45 +0900

    _

--- 2580/main.py
+++ 2580/main.py
@@ -1,3 +1,48 @@
+# import sys
+#
+# board = []
+# elements = set(range(10))
+#
+# for _ in range(9):
+#     board.append(list(map(int, sys.stdin.readline().split())))
+#
+# def sudoku(row):
+#     if row == 9:
+#         for i in board:
+#             print(*i)
+#         exit(0)
+#
+#     # 빈 숫자
+#     blank_nums = elements - set(board[row])
+#     col = board[row].index(0)
+#     for i in blank_nums:
+#         board[row][col] = i
+#         if promising(row, col):
+#             if 0 not in board[row]:
+#                 sudoku(row+1)
+#             else:
+#                 sudoku(row)
+#
+#         board[row][col] = 0
+#
+# def promising(row, col):
+#     check = set()
+#     for i in range(9):
+#         if board[i][col] != 0 and board[i][col] in check:
+#             return False
+#         check.add(board[i][col])
+#     check.clear()
+#
+#     for i in range(row//3*3, row//3*3 + 3):
+#         for j in range(col//3*3, col//3*3 + 3):
+#             if board[i][j] != 0 and board[i][j] in check:
+#                 return False
+#             check.add(board[i][j])
+#
+#     return True
+#
+# sudoku(0)
+#
 import sys
 
 board = []
@@ -11,30 +56,30 @@ def sudoku(row):
         for i in board:
             print(*i)
         exit(0)
-
     # 빈 숫자
-    blank_nums = elements - set(board[row])
+    row_blank_nums = elements - set(board[row])
     col = board[row].index(0)
+    col_blank_nums = set()
+    for i in range(9):
+        if board[i][col] != 0 and board[i][col] in col_blank_nums:
+            return False
+        col_blank_nums.add(board[i][col])
+
+    blank_nums = row_blank_nums & col_blank_nums
     for i in blank_nums:
         board[row][col] = i
         if promising(row, col):
             if 0 not in board[row]:
-                sudoku(row+1)
+                sudoku(row + 1)
             else:
                 sudoku(row)
 
         board[row][col] = 0
+    return
 
 def promising(row, col):
-    check = set()
-    for i in range(9):
-        if board[i][col] != 0 and board[i][col] in check:
-            return False
-        check.add(board[i][col])
-    check.clear()
-
-    for i in range(row//3*3, row//3*3 + 3):
-        for j in range(col//3*3, col//3*3 + 3):
+    for i in range(row // 3 * 3, row // 3 * 3 + 3):
+        for j in range(col // 3 * 3, col // 3 * 3 + 3):
             if board[i][j] != 0 and board[i][j] in check:
                 return False
             check.add(board[i][j])
